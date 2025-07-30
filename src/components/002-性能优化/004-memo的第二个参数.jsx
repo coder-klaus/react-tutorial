ad0348e1bd844b0013c89e92191b5a3a6816f8e4
klaus <klauswang.2018@gmail.com>
2025-07-30 19:03:28 +0800

    fix: 同步笔记

--- src/components/002-性能优化/004-memo的第二个参数.jsx
+++ src/components/002-性能优化/004-memo的第二个参数.jsx
@@ -5,12 +5,11 @@ const Counter = memo(
 ({ counter }) => {
   return <div>{ counter }</div>
 },
-// memo的第二个参数是对比函数，它接收两个参数：prevProps 和 nextProps。
-// 如果返回 true，表示 props 没有变化，不需要重新渲染；返回 false，则会重新渲染。
-// ⚠️ 其返回结果和 SCU的结果是反的，返回true表示不更新，返回false表示更新
-(prevProps, nextProps) => {
-  return false
-})
+// memo的第二个参数是对比函数 「 (prevProps, nextProps) => boolean 」
+// + 它接收两个参数：prevProps 和 nextProps。
+// + 如果返回 true，表示 props 没有变化，不需要重新渲染；返回 false，则会重新渲染。
+// + ⚠️ 其返回结果和 SCU的结果是反的，返回true表示不更新，返回false表示更新
+() => false)
 
 class App extends PureComponent {
   state = {
