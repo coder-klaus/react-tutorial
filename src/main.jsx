44480a185b6d5add3266fdccac2daea0015a7144
klaus <klauswang.1030@gmail.com>
2025-08-20 14:39:40 +0800

    fix: 同步示例代码

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
+// 导入严格模式
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  // 使用严格模式
   <StrictMode>
     <App />
   </StrictMode>,
