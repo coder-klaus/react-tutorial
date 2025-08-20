9179c86f539a4761463372a81cb6481e9bd81ff7
klaus <klauswang.1030@gmail.com>
2025-08-20 16:50:46 +0800

    fix: 同步react-redux 使用示例

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
-import { memo, useState, useEffect } from 'react'
-import store from './store'
+import { memo } from 'react'
+// react-redux 只是简化了 redux 在组件中的使用，并不影响 redux 本身的定义方式
+import { connect } from 'react-redux'
 import { increment, decrement } from './store/actionCreators'
 
-const App = memo(() => {
-  const [_, forceUpdate] = useState(0)
-
-  const { count } = store.getState()
-  useEffect(() => {
-    const unsubscribe = store.subscribe(() => {
-      const { count: currentCount } = store.getState()
-      if (currentCount !== count) {
-        forceUpdate(prev => prev + 1)
-      }
-    })
-    return unsubscribe
-  }, [count])
+const App = memo((props) => {
+  const { count, increment, decrement } = props
 
   return (
     <>
       <h1>{count}</h1>
-      <button onClick={() => store.dispatch(increment())}>+1</button>
-      <button onClick={() => store.dispatch(decrement())}>-1</button>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
     </>
   )
 })
 
-export default App
+// 全局注入后，组件就不需要自己导入state，并调用 getState() 获取状态对象
+// mapStateToProps 是一个函数，用于将 state 映射到组件的 props 中
+// 参数为 redux state 对象，返回值为 props 对象
+const mapStateToProps = (state) => {
+  return {
+    count: state.count
+  }
+}
+
+// mapDispatchToProps 是一个函数，用于将 dispatch 映射到组件的 props 中
+// 参数为 dispatch 函数，返回值为 props 对象
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch(increment()),
+    decrement: () => dispatch(decrement())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import store from './store/index.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* react-redux 提供 Provider 组件，用于将 store 注入到应用中, 避免每个组件在使用时都需要单独导入 store */}
+    {/* react-redux 底层使用的依旧是 react的 context机制 */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
