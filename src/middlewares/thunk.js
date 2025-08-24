77b08a9e854bfa80b43db3a37943ea3a6bd16800
klaus <klauswang.1030@gmail.com>
2025-08-24 15:00:43 +0800

    fix: add case

--- src/middlewares/thunk.js
+++ src/middlewares/thunk.js
@@ -2,7 +2,6 @@
 export default store => next => action => {
   // 如果 action 是一个函数，则直接调用该函数，并传入 dispatch 和 getState
   if (typeof action === 'function') {
-    // 如果异步函数返回的还是一个函数，则嵌套调用 => 实现异步函数嵌套调用
     return action(store.dispatch, store.getState);
   }
 
